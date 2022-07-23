9e1efadfd5c115a50bdb1b746545584961a36b1a
Alexander Quezada <alfredq11@live.com>
2022-07-23 00:21:47 -0700

    Updated Layout for foot color. Edited Contact me mailto link. Sorted skills lists to create flow on page.

---
+++ Portfolio/Models/PortfolioSection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Models
+{
+    /**
+     * This Model will be used to add and remove sections
+     * of my portfolio. I can for example replace all the 
+     * current HTML with a for each loop that runs through 
+     * a list of predefined sections.
+     *
+     */
+    public class PortfolioSection
+    {
+        public int ID { get; set; }
+        public string Title { get; set; } = string.Empty;
+    }
+}
